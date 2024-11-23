5c42cf8c092161089e1f0a285cfe03085672b6e4
technokerala <ngr01akash@gmail.com>
2024-11-23 21:22:26 +0530

    Page Splitted to Home, State Dashboaed and Footer

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,15 @@
+import { styles } from "../Styles";
+
+function Footer() {
+  return (
+    <div>
+      {/* Footer */}
+      <footer style={styles.footer}>
+        <p>Data Powered by Information Kerala Mission</p>
+        <p>Contact: mail@ikm.gov.in</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
